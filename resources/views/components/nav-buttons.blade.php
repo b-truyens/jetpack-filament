85eef77e5b6bb96286736048467caca59ec8c4b2
b-truyens <boristruyens@protonmail.com>
2023-06-21 16:13:36 +0200

    Fix Nav Buttons and Logo.

--- resources/views/components/nav-buttons.blade.php
+++ resources/views/components/nav-buttons.blade.php
@@ -1,19 +1,22 @@
-<div class="inline-flex" role="group">
-<div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
-    @auth
-        <div class="navigation-link">
-            <a href="{{ url('/dashboard') }}">Dashboard</a>
-            <a href="{{ url('/admin') }}">Admin</a>
-        </div>
-    @else
-        <div class="navigation-link">
-            @if (Route::has('login'))
-                <a href="{{ route('login') }}">Log in</a>
-            @endif
+<div class="inline-flex fixed top-0 right-0 p-6 text-right z-10" role="group">
+    @if (Route::has('login'))
+        @auth
+            <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
+                Dashboard
+            </a>
+            <a href="{{ url('/admin') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
+                Admin
+            </a>
+        @else
+            <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
+                Log in
+            </a>
+
             @if (Route::has('register'))
-                <a href="{{ route('register') }}">Register</a>
+                <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
+                    Register
+                </a>
             @endif
-        </div>
-    @endauth
-    </div>
+        @endauth
+    @endif
 </div>
---
+++ resources/views/layouts/blog.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+        <!-- Styles -->
+        <style>
+        </style>
+
+    </head>
+    <body class="antialiased">
+        <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
+
+            <x-nav-bar />
+
+            <x-logo />
+
+                <div class="mt-16">
+
+                    <div class="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
+
+                        <x-post />
+                        <x-post1 />
+                        <x-post2 />
+                        <x-post-div />
+ 
+                    </div>
+                </div>
+
+                    <x-footer />
+            </div>
+    </body>
+</html>
